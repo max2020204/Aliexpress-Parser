9d9704344e1d071d6610cac0f8f639e64e994e3f
blackdreamms <blackdreamms@outlook.com>
2020-10-10 22:27:05 +0300

    Добавьте файлы проекта.

---
+++ AliInfo/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace AliInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(807, 48);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(649, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(719, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(807, 299);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(801, 280);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 347);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ AliInfo/Form1.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AliInfo
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver Browser;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var item in richTextBox1.Lines)
+            {
+                string priceRUB, priceUAH, priceUSD, mark, review, orders, rating;
+                Browser = new ChromeDriver();
+                Browser.Navigate().GoToUrl(item);
+                priceRUB = Browser.FindElement(By.ClassName("product-price-value")).Text;
+                mark = Browser.FindElement(By.ClassName("product-price-mark")).Text;
+                review = Browser.FindElement(By.ClassName("product-reviewer-reviews")).Text;
+                review = review.Replace("Отзывы", "");
+                orders = Browser.FindElement(By.ClassName("product-reviewer-sold")).Text;
+                orders = orders.Replace("заказа(ов)", "");
+                rating = Browser.FindElement(By.ClassName("overview-rating-average")).Text;
+                Cookie cookie = Browser.Manage().Cookies.GetCookieNamed("aep_usuc_f");
+                string val = cookie.Value;
+                val = val.Replace("RUB", "USD");
+                Browser.Manage().Cookies.DeleteCookieNamed("aep_usuc_f");
+                Browser.Manage().Cookies.AddCookie(new Cookie("aep_usuc_f", val));
+                Browser.Navigate().Refresh();
+                Thread.Sleep(1000);
+                priceUSD = Browser.FindElement(By.ClassName("product-price-value")).Text;
+                Browser.Manage().Cookies.DeleteCookieNamed("aep_usuc_f");
+                val = val.Replace("USD", "UAH");
+                Browser.Manage().Cookies.AddCookie(new Cookie("aep_usuc_f", val));
+                Browser.Navigate().Refresh();
+                Thread.Sleep(1000);
+                priceUAH = Browser.FindElement(By.ClassName("product-price-value")).Text;
+                richTextBox1.Text = $"Цена: {priceRUB.Replace("руб.", "")} | {priceUAH.Replace("грн.", "")} | {priceUSD.Replace("US $", "")} \n" +
+                                $"Скидка: {mark} \n" +
+                                $"Заказов: {orders}\n" +
+                                $"Рейтинг продавца: {rating}\n" +
+                                $"Отзывов: {review}\n";
+                Browser.Close();
+                Browser.Dispose();
+
+            }
+        }
+    }
+}
